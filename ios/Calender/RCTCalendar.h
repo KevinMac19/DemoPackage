165a1405f80fc99df9e09c1b10c99487707912bf
KevinMac19 <kevin_macwan@codenticsoftware.com>
2022-03-23 15:17:17 +0530

    Demo Calender component added

---
+++ ios/Calender/RCTCalendar.h
@@ -0,0 +1,16 @@
+//
+//  RCTCalender.h
+//  DemoPackage
+//
+//  Created by CS006 on 23/03/22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+//  RCTCalendarModule.h
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+
+@end
+
+
+#endif /* RCTCalender_h */
---
+++ ios/Calender/RCTCalendar.m
@@ -0,0 +1,14 @@
+#import "RCTCalendar.h"
+#import <React/RCTLog.h>
+
+@implementation RCTCalendarModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE(RCTCalendar)
+
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+{
+ RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+@end
